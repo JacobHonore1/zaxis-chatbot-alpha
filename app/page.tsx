bfd1fa3ec78bad1c3045b7f584081b971f669700
JacobHonore1 <139641640+JacobHonore1@users.noreply.github.com>
2025-10-15 11:46:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -42,12 +42,13 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            prefetch={false}
-            href="/chat"
-          >
-            Åbn Zaxis AI Chat
-          </Link>
+  className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+  prefetch={false}
+  href="/chat"
+>
+  Åbn Zaxis AI Chat
+</Link>
+
 
           <a
             href="https://vercel.com/templates/ai/chatgpt-app-with-next-js"
